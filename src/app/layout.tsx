8921996c7c329a74b213615cb00e20d8fe634f98
elvissamuel <77748602+elvissamuel@users.noreply.github.com>
2024-02-29 23:59:15 +0100

    add latest

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,12 +7,11 @@ import '@/styles/tailwind.css'
 
 export const metadata: Metadata = {
   title: {
-    template: '%s - Spencer Sharp',
-    default:
-      'Spencer Sharp - Software designer, founder, and amateur astronaut',
+    template: '%s - Tunde Adeniran',
+    default: 'Tunde Adeniran - Co-Founder, Product Leader, & Innovator',
   },
   description:
-    'I’m Spencer, a software designer and entrepreneur based in New York City. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms.',
+    'I am Tunde Adeniran, a seasoned product leader with a passion for innovation and growth.',
   alternates: {
     types: {
       'application/rss+xml': 'https://example.com/feed.xml',
